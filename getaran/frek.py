f0af3660a3ba800291bc1d0a988038f2a098d206
akbarpn136 <akbarpn136@gmail.com>
2024-01-14 15:07:42 +0700

    Daftar command baru

--- getaran/frek.py
+++ getaran/frek.py
@@ -1,5 +1,6 @@
 from typer import Typer
 
+from getaran.command.hitung_fm import fmz
 from getaran.command.hitung_lsce import lsce
 from getaran.command.plot_getaran import plotgtr
 from getaran.command.olah_riv import waterfall, displacement
@@ -24,3 +25,7 @@ app.command(
 app.command(
     help="Menghitung displacement dari data RIV."
 )(displacement)
+
+app.command(
+    help="Menghitung flutter margin dengan zimmerman."
+)(fmz)
